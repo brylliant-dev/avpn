f1a212fd4fe39137f6d7a3d368f3c4f687f27384
Jemuel Lupo <jemuel.lupo@sb2024.live>
2024-10-11 19:36:40 +0800

    refactor(): move js to repo

---
+++ bubble-chart-1.js
@@ -0,0 +1,147 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chartContainer = document.getElementById("bubble-chart-1");
+    let myChart = null; // Variable to hold the bubble chart instance
+    
+    // Function to initialize the bubble chart
+    function initializeBubbleChart() {
+        if (myChart) {
+            myChart.destroy();
+        }
+
+        const chartData = [
+            { label: 'Ageing', percentage: 16 },
+            { label: 'Agriculture', percentage: 33 },
+            { label: 'Arts and Culture', percentage: 19 },
+            { label: 'Affordable Housing', percentage: 17 },
+            { label: 'Climate Action & Environment', percentage: 58 },
+            { label: 'Conservation', percentage: 58 },
+            { label: 'Education', percentage: 63 },
+            { label: 'Employability', percentage: 38 },
+            { label: 'Energy', percentage: 30 },
+            { label: 'Financial Inclusion', percentage: 34 },
+            { label: 'Gender', percentage: 38 },
+            { label: 'Governance', percentage: 20 },
+            { label: 'Health', percentage: 52 },
+            { label: 'Livelihood & Poverty Alleviation', percentage: 51 },
+            { label: 'Nutrition', percentage: 23 },
+            { label: 'Water, Sanitation & Hygiene', percentage: 29 },
+        ]
+
+        const maxRadius = window.innerWidth < 480 ? 20 : 40;
+        const minRadius = window.innerWidth < 480 ? 5 : 10;
+        const scaleRadius = (percentage) => {
+            const scale = (maxRadius - minRadius) / 100;
+            return minRadius + (percentage * scale);
+        };
+
+        const labels = chartData.map(cd => cd.label)
+        const data = {
+            labels,
+            datasets: [
+                {
+                    label: 'Percentage',
+                    data: chartData.map(cd => cd.percentage).map((percentage, index) => ({
+                        x: index,
+                        y: percentage,
+                        r: scaleRadius(percentage)
+                    })),
+                    backgroundColor: "#f27c38",
+                    borderWidth: 1,
+                    borderColor: "#E8F6FF",
+                    hoverBackgroundColor: "#f27c38"
+                }
+            ]
+        };
+        const isMobile = window.innerWidth < 480;
+        const xFontSize = isMobile ? 8 : 14; // Smaller font size for mobile
+        const maxRotation = isMobile ? 90 : 45; // Increase rotation for better readability on mobile
+        const paddingBottom = isMobile ? 140 : 40; // Increase bottom padding for mobile
+        myChart = new Chart(chartContainer, {
+            type: 'bubble',
+            data: data,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 10,
+                        top: 10,
+                        bottom: paddingBottom
+                    }
+                },
+                scales: {
+                    x: {
+                        title: { display: false },
+                        grid: { display: true, drawBorder: true },
+                        ticks: {
+                            color: '#002944',
+                            font: { family: 'Open Sans', size: xFontSize }, // Adjust font size based on screen width
+                            min: 0,
+                            max: labels.length - 1,
+                            stepSize: 1,
+                            autoSkip: false,
+                            callback: function (value) { return labels[value]; },
+                            maxRotation: maxRotation, // Adjust rotation for better readability
+                            minRotation: 0
+                        }
+                    },
+                    y: {
+                        title: { display: false },
+                        grid: { display: true, drawBorder: true, drawOnChartArea: true },
+                        ticks: {
+                            color: '#002944',
+                            font: { family: 'Open Sans', size: xFontSize }, // Adjust font size based on screen width
+                            callback: function (value) { return value + '%'; },
+                            beginAtZero: true,
+                            max: 70
+                        }
+                    }
+                },
+                plugins: {
+                    legend: { display: false },
+                    tooltip: {
+                        titleFont: {
+                            family: 'Open Sans',
+                            color: '#002944'
+                        },
+                        bodyFont: {
+                            family: 'Open Sans',
+                            color: '#002944'
+                        },
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                const label = labels[tooltipItem.dataIndex];
+                                const percentage = tooltipItem.raw.y;
+                                return `${label}: ${percentage}%`;
+                            }
+                        }
+                    }
+                },
+                animation: {
+                    duration: 2000,
+                    delay: function (context) {
+                        const index = context.dataIndex;
+                        const delayBetweenPoints = 50;
+                        return index * delayBetweenPoints;
+                    }
+                }
+            }
+        });
+    }
+    // Function to observe when Swiper slide becomes active
+    function observeSwiperSlideChanges() {
+        const swiperSlides = document.querySelectorAll('.review_members_slide.swiper-slide');
+        swiperSlides.forEach((slide, index) => {
+            swiper.on('transitionEnd', function () {
+                if (slide.classList.contains('swiper-slide-active')) {
+                    if (slide.contains(chartContainer)) {
+                        initializeBubbleChart()
+                    }
+                }
+            });
+        });
+    }
+    // Start observing Swiper slide changes
+    observeSwiperSlideChanges();
+})
---
+++ polar-area-1.js
@@ -0,0 +1,128 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chartContainer = document.getElementById("polar-area-1");
+    let myChart = null; // Variable to hold the polar area chart instance
+
+    // Function to initialize the polar area chart
+    function initializePolarAreaChart() {
+        if (myChart) {
+            myChart.destroy();
+        }
+
+        const chartData = [
+            { label: 'Children and youths', color: '#00b4ae', number: 65 },
+            { label: 'Elderly', color: '#f27c38', number: 28 },
+            { label: 'Environment', color: '#ffd552', number: 44 },
+            { label: 'Ethnic minorities', color: '#007b69', number: 27 },
+            { label: ['Immigrants and asylum', 'seekers and refugees'], color: '#39627a', number: 14 },
+            { label: ['Offenders and', 're-offenders'], color: '#86d1d1', number: 6 },
+            { label: 'People in poverty', color: '#f59d6a', number: 58 },
+            { label: ['People with', 'disabilities'], color: '#fee07e', number: 29 },
+            { label: ['People with', 'medical needs'], color: '#80bdb4', number: 30 },
+            { label: ['People without', 'employment'], color: '#9cb1bd', number: 37 },
+            { label: 'Women and girls', color: '#c0deda', number: 62 },
+        ]
+
+        const labels = [
+            'Children and youths',
+            'Elderly',
+            'Environment',
+            'Ethnic minorities',
+            ['Immigrants and asylum', 'seekers and refugees'],
+            ['Offenders and', 're-offenders'],
+            'People in poverty',
+            ['People with', 'disabilities'],
+            ['People with', 'medical needs'],
+            ['People without', 'employment'],
+            'Women and girls'
+        ]; // We can keep this in the code for references
+        const colors = [
+            '#00b4ae', '#f27c38', '#ffd552', '#007b69', '#39627a',
+            '#86d1d1', '#f59d6a', '#fee07e', '#80bdb4', '#9cb1bd',
+            '#c0deda', '#f9be9b', '#fee9a9', '#ced8de', '#c1e6e7'
+        ]; // We can keep this in the code for references
+
+        const chartNumbers = [65, 28, 44, 27, 14, 6, 58, 29, 30, 37, 62] // We can keep this in the code for references
+
+        const chartDataSorted = chartData.sort((a, b) => a.number > b.number ? -1 : 1) // Sort data from biggest to smallest number
+
+        const data = {
+            labels: chartDataSorted.map(cds => cds.label),
+            datasets: [
+                {
+                    label: 'Percentage',
+                    data: chartDataSorted.map(cds => cds.number),
+                    backgroundColor: chartDataSorted.map(cds => cds.color).slice(0, labels.length),
+                    borderWidth: 1,
+                    borderColor: "#E8F6FF",
+                    borderRadius: 4,
+                    hoverBackgroundColor: colors.slice(0, labels.length)
+                }
+            ]
+        };
+        const fontSize = window.innerWidth < 480 ? 10 : 16;
+        myChart = new Chart(chartContainer, {
+            type: 'polarArea',
+            data: data,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    r: {
+                        pointLabels: {
+                            display: true,
+                            centerPointLabels: true,
+                            color: '#002944',
+                            font: {
+                                size: fontSize,
+                                family: 'Open Sans'
+                            }
+                        },
+                        ticks: {
+                            display: false
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    tooltip: {
+                        titleFont: {
+                            family: 'Open Sans',
+                            color: '#002944'
+                        },
+                        bodyFont: {
+                            family: 'Open Sans',
+                            color: '#002944'
+                        },
+                        callbacks: {
+                            label: function (tooltipItem) {
+                                return tooltipItem.raw + '%';
+                            }
+                        }
+                    }
+                },
+                animation: {
+                    animateRotate: true,
+                    duration: 2000
+                }
+            }
+        });
+    }
+
+    function observeSwiperSlideChanges() {
+        const swiperSlides = document.querySelectorAll('.review_members_slide.swiper-slide');
+        swiperSlides.forEach((slide, index) => {
+            swiper.on('transitionEnd', function () {
+                if (slide.classList.contains('swiper-slide-active')) {
+                    if (slide.contains(chartContainer)) {
+                        initializePolarAreaChart()
+                    }
+                }
+            });
+        });
+    }
+    // Start observing Swiper slide changes
+    observeSwiperSlideChanges();
+    
+})
